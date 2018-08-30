bb2694ca4767768960395a3ee3739dd14b655716
Tejas-Nanaware <nanawaret@hotmail.com>
2018-08-30 20:55:54 +0530

    Fixed incorrect name on accordion

--- visualizations.php
+++ visualizations.php
@@ -67,7 +67,7 @@ console.log(v3);
     <div class="card-header" id="headingOne">
       <h5 class="mb-0">
         <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Drug Type
+          Drug Problem
         </button>
       </h5>
     </div>
