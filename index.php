1c556141db4f3f1e60608ba189062b006058a72d
Tejas-Nanaware <nanawaret@hotmail.com>
2018-09-01 16:02:19 +0530

    Fixed hyperlink to map

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 	<p>Hi</p>
 	<p><a href='filteredreports.php'>Filtered Data</a></p>
 	<p><a href='visualizations.php'>Visualised Data</a></p>
+	<p><a href='tempmap.php'>Map</a></p>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
